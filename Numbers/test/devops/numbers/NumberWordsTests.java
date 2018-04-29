6307112566861513872f3d8f75de47f9a879384d
shubham88888 <31254804+shubham88888@users.noreply.github.com>
2018-04-29 19:08:00 +0530

    Update NumberWordsTests.java

--- Numbers/test/devops/numbers/NumberWordsTests.java
+++ Numbers/test/devops/numbers/NumberWordsTests.java
@@ -5,16 +5,33 @@ import org.junit.*;
 import devops.numbers.NumberWords;
 
 public class NumberWordsTests {
-	private NumberWords numberWords ;
 	
-	@Before
-	public void setup() {
-		numberWords = new NumberWords() ;
-	}
-
+	NumberWords numberWord = new NumberWords();
 	@Test
-	public void numberOutOfRangeReturnsError() {
-		Assert.assertEquals( "Error", "Number out of range", numberWords.toWords( -1 ) ) ;
+	public void c
+	() {
+		Assert.assertEquals( "MinusOne", numberWord.convert( -1 ) ) ;
 	}
+	@Test
+	public void testCase1() {
+		Assert.assertEquals( "Fifty One", numberWord.convert( 51 ) ) ;
+	}
+	@Test
+	public void testCase2() {
+		Assert.assertEquals( "One Hundred", numberWord.convert( 100 ) ) ;
+	}
+	@Test
+	public void testCase3() {
+		Assert.assertEquals( "Five Hundred Seventy Two", numberWord.convert( 572 ) ) ;
+	}
+	@Test
+	public void testCase4() {
+		Assert.assertEquals( "One Thousand Two Hundred", numberWord.convert( 1200 ) );
+	}
+	@Test
+	public void testCase5() {
+		Assert.assertEquals( "Ninety Lakh",  numberWord.convert( 9000000 ) ) ;
+	}
+	
 
 }
